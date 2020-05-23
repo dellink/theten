e62816c04ebea5dbd1f93855c2cdff536784bae8
Serge Adamovich <adamovichserge@gmail.com>
2020-05-23 14:54:35 +0300

    Add tests for moveCell

--- src/game-logic.test.js
+++ src/game-logic.test.js
@@ -1,5 +1,5 @@
 import { tiles } from '../mocks';
-import { clear10, generateTiles, isHasMove } from './game-logic';
+import { clear10, generateTiles, isHasMove, moveCell } from './game-logic';
 
 describe('game logic', () => {
   it(`${generateTiles.name} should generate tiles`, () => {
@@ -25,5 +25,41 @@ describe('game logic', () => {
 
     expect(res.length).toBe(tiles.length + 1);
     expect(res[0].mergedInto).not.toBeNull();
+  });
+
+  it(`${moveCell.name} should move a tile left`, () => {
+    const sourceTiles = tiles.map(tile => ({ ...tile }));
+    const res = moveCell(sourceTiles, 0, tiles[3]);
+
+    expect(res[2].mergedInto).not.toBeNull();
+    expect(res[2].mergedInto.value).toEqual(sourceTiles[2].value + sourceTiles[3].value);
+    expect(res[2].mergedInto).toEqual(res[3].mergedInto);
+  });
+
+  it(`${moveCell.name} should move a tile up`, () => {
+    const sourceTiles = tiles.map(tile => ({ ...tile }));
+    const res = moveCell(sourceTiles, 1, tiles[6]);
+
+    expect(res[2].mergedInto).not.toBeNull();
+    expect(res[2].mergedInto.value).toEqual(sourceTiles[2].value + sourceTiles[6].value);
+    expect(res[2].mergedInto).toEqual(res[6].mergedInto);
+  });
+
+  it(`${moveCell.name} should move a tile right`, () => {
+    const sourceTiles = tiles.map(tile => ({ ...tile }));
+    const res = moveCell(sourceTiles, 2, tiles[6]);
+
+    expect(res[6].mergedInto).not.toBeNull();
+    expect(res[6].mergedInto.value).toEqual(sourceTiles[6].value + sourceTiles[7].value);
+    expect(res[6].mergedInto).toEqual(res[7].mergedInto);
+  });
+
+  it(`${moveCell.name} should move a tile down`, () => {
+    const sourceTiles = tiles.map(tile => ({ ...tile }));
+    const res = moveCell(sourceTiles, 3, tiles[6]);
+
+    expect(res[6].mergedInto).not.toBeNull();
+    expect(res[6].mergedInto.value).toEqual(sourceTiles[6].value + sourceTiles[10].value);
+    expect(res[6].mergedInto).toEqual(res[10].mergedInto);
   });
 });
